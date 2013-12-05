311025bd5e69f90a667eff7806cbd1e3f200ca00
Marcio C Souza <m4rcio.souza@gmail.com>
2013-12-05 12:57:38 -0500

    First app setup
    Created the app using yo angular, added grunt-karma to project and
    edited Gruntfile.js to config the grunt-karma that was missing, added
    ‘grunt.loadNpmTasks('grunt-karma');’ to setup correctly the tests.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('kzbmcMobileApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
