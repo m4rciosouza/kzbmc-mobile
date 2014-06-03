762b3165952a17f61ab9b63e301f0cae6ffb8103
Marcio C Souza <m4rcio.souza@gmail.com>
2014-06-02 22:08:01 -0300

    Refactored the help views to be mobile friendly.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,6 +29,36 @@ kzbmcMobileApp.config(function ($routeProvider) {
         templateUrl: 'views/canvas.html',
         controller: 'CanvasCtrl'
       })
+      .when('/ajuda', {
+        templateUrl: 'views/ajuda/index.html'
+      })
+      .when('/ajuda/segmentos-clientes', {
+        templateUrl: 'views/ajuda/segmentos-clientes.html'
+      })
+      .when('/ajuda/propostas-valor', {
+        templateUrl: 'views/ajuda/propostas-valor.html'
+      })
+      .when('/ajuda/canais', {
+        templateUrl: 'views/ajuda/canais.html'
+      })
+      .when('/ajuda/relacionamento-clientes', {
+        templateUrl: 'views/ajuda/relacionamento-clientes.html'
+      })
+      .when('/ajuda/recursos-chave', {
+        templateUrl: 'views/ajuda/recursos-chave.html'
+      })
+      .when('/ajuda/atividades-chave', {
+        templateUrl: 'views/ajuda/atividades-chave.html'
+      })
+      .when('/ajuda/parceiros-chave', {
+        templateUrl: 'views/ajuda/parceiros-chave.html'
+      })
+      .when('/ajuda/estrutura-custo', {
+        templateUrl: 'views/ajuda/estrutura-custo.html'
+      })
+      .when('/ajuda/fluxo-receita', {
+        templateUrl: 'views/ajuda/fluxo-receita.html'
+      })
       .otherwise({
         redirectTo: '/'
       });
